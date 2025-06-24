a58939628ea144d9d7949c15401ac1dfd700d763
jswikarta <jswikarta@gmail.com>
2025-06-24 12:05:10 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/deploy-chat",
+  base: "/deploy-chat/",
   plugins: [react(), tailwindcss()],
 });
